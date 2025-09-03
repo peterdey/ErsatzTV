a83cb672784a910ade905d6a0c7f6ff5b6d059e8
Peter Dey <github@realmtech.net>
2025-09-03 21:51:43 +0200

    Update DecoderRkmpp to reflect it's a hardware decoder, and provide correct pixel format

--- ErsatzTV.FFmpeg/Decoder/DecoderRkmpp.cs
+++ ErsatzTV.FFmpeg/Decoder/DecoderRkmpp.cs
@@ -1,8 +1,24 @@
-﻿namespace ErsatzTV.FFmpeg.Decoder;
+﻿using ErsatzTV.FFmpeg.Format;
+
+namespace ErsatzTV.FFmpeg.Decoder;
 
 public class DecoderRkmpp : DecoderBase
 {
-    protected override FrameDataLocation OutputFrameDataLocation => FrameDataLocation.Software;
+    protected override FrameDataLocation OutputFrameDataLocation => FrameDataLocation.Hardware;
+
     public override string Name => "implicit_rkmpp";
-    public override string[] InputOptions(InputFile inputFile) => Array.Empty<string>();
+
+    public override string[] InputOptions(InputFile inputFile) =>
+        new[] { "-hwaccel_output_format", "drm_prime" };
+
+    public override FrameState NextState(FrameState currentState)
+    {
+        FrameState nextState = base.NextState(currentState);
+
+        return currentState.PixelFormat.Match(
+            pixelFormat => pixelFormat.BitDepth == 8
+                ? nextState with { PixelFormat = new PixelFormatNv12(pixelFormat.Name) }
+                : nextState with { PixelFormat = new PixelFormatP010() },
+            () => nextState);
+    }
 }
